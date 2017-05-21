60a5c0b17dfdd436877fdcb404475ab4f69ca6e5
Michał Wąsowicz <mwasowicz7@gmail.com>
2017-05-21 11:32:02 +0200

    created BasePoorManager

---
+++ app/src/main/java/com/bearcave/passageplanning/base/BasePoorManagerFragment.java
@@ -0,0 +1,53 @@
+package com.bearcave.passageplanning.base;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ExpandableListView;
+
+import com.bearcave.passageplanning.R;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+
+/**
+ * BaseManager fragment without FloatingActionButton.
+ */
+public abstract class BasePoorManagerFragment extends BaseFragment {
+
+    private Unbinder unbinder;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_base_poor_manager, container, false);
+
+        ExpandableListView listView = ButterKnife.findById(view, R.id.list_view);
+        listView.setAdapter(getAdapter());
+
+        unbinder = ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+
+    protected abstract BaseManagerAdapter getAdapter();
+}
